05c12d8026e9bdf58d38f492e366ecbabbea1d45
Alex Corrado <alexc@xamarin.com>
2016-05-11 21:29:27 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace MacDaemonScaffold {
+	class MainClass {
+		public static void Main (string [] args)
+		{
+			NSApplication.Init ();
+			var alert = new NSAlert {
+				MessageText = "Hello world!"
+			};
+			alert.RunModal ();
+		}
+	}
+}
